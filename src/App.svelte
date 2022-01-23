95ca5a65f04e0bd51adf7bee814866074eb88eff
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2022-01-23 16:04:40 -0500

    extend window type with ethereum

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,6 @@
     if ('ethereum' in window) {
       isEtherPresent = true
 
-      // @ts-ignore
       const provider = new ethers.providers.Web3Provider(window.ethereum)
 
       await provider.send('eth_requestAccounts', [])
