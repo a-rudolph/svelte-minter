7ee1bdf7000ce1a0bd6cce4b6c8893277bb4d8c7
a-rudolph <adam.rudolph@live.ca>
2022-01-25 19:42:41 -0500

    slide the cards

--- src/lib/MintCard.svelte
+++ src/lib/MintCard.svelte
@@ -8,13 +8,19 @@
   import Token from './Token.svelte'
 </script>
 
-<div in:fly="{{ x: -200, duration: 1000 }}" class="m-2 p-5 rounded shadow-lg border-2">
+<div
+  in:fly="{{ x: -300, duration: 500 }}"
+  class="m-2 p-5 rounded shadow-lg border-2">
   <Token {id} {isMinted} />
   <div class="mt-5 w-full flex justify-end">
-  {#if isMinted}
-  <p>{tag}</p>
-  {:else}
-  <button on:click={onMint} class="px-3 py-1 bg-green-800 text-white rounded-md">mint</button>
-  {/if}
-</div>
+    {#if isMinted}
+      <p>{tag}</p>
+    {:else}
+      <button
+        on:click={onMint}
+        class="px-3 py-1 bg-green-800 text-white rounded-md">
+        mint
+      </button>
+    {/if}
+  </div>
 </div>
--- src/lib/Minter.svelte
+++ src/lib/Minter.svelte
@@ -1,6 +1,28 @@
 <script>
   import MintCard from './MintCard.svelte'
 
+  import { quintOut } from 'svelte/easing';
+	import { crossfade } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
+
+	const [send, receive] = crossfade({
+		duration: d => Math.sqrt(d * 200),
+
+		fallback(node, params) {
+			const style = getComputedStyle(node);
+			const transform = style.transform === 'none' ? '' : style.transform;
+
+			return {
+				duration: 600,
+				easing: quintOut,
+				css: t => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`
+			};
+		}
+	});
+
   const maxis = {
     chin: 2,
     ears: 2,
@@ -34,9 +56,11 @@
     idsMinted[id] = 1
     return id
   }
-
   const idsMinted = {}
-  let mintedIds = []
+
+  let current = getId()
+
+  let mintedIds = [current]
 
   const onMint = () => {
     const id = getId()
@@ -54,11 +78,19 @@
     {mintedIds.length} / {possibilities} possible tokens
   </p>
   <div class="w-full flex flex-wrap justify-center sm:justify-start">
-    {#if areMintablesLeft}
-    <MintCard {onMint} />      
-    {/if}
     {#each mintedIds as id, i (id)}
-      <MintCard {id} isMinted tag="{mintedIds.length - i}/{possibilities}" />
+      <div
+        in:receive="{{key: id}}"
+        out:send="{{key: id}}"
+        animate:flip="{{duration: 200}}"
+      >
+        <MintCard
+          {id}
+          {onMint}
+          isMinted={i > 0}
+          tag="{mintedIds.length - i}/{possibilities}"
+        />
+      </div>
     {/each}
   </div>
 </div>
